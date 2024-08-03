9ffef9bfa31d829093b0e32ec47cd509e3350714
JesseBCS <j.kilbride@bearcloudstudios.com>
2024-08-03 17:59:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-s# Import python packages
+# Import python packages
 import streamlit as st
-
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,8 +11,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+
+session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
-    max_selections=6
+    max_selections=5
     )
 
 if ingredients_list:
@@ -44,5 +44,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!' + ', '+name_on_order, icon="✅")
-
-
